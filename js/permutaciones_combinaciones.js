d44c65b8a02721b409d36ced405685f97f58d491
Carlos Alfonso Barrón Rivera <carloslego.br@gmail.com>
2021-12-17 12:51:08 -0600

    funcionalidad pagina 2 y 3

--- js/permutaciones_combinaciones.js
+++ js/permutaciones_combinaciones.js
@@ -1,3 +1,5 @@
+let operacion = "";
+
 function muestraperm(){
   document.getElementById("perm").style.display  = "";
   document.getElementById("comb").style.display  = "none";
@@ -5,6 +7,8 @@ function muestraperm(){
   document.getElementById("resultado").value  = "0";
   document.getElementById("n").value  = "";
   document.getElementById("r").value  = "";
+  document.getElementById("resultado").innerHTML = "Resultado :"
+  operacion = 1;
 
 }
 
@@ -15,4 +19,29 @@ function muestracomb(){
   document.getElementById("resultado").value  = "0";
   document.getElementById("n").value  = "";
   document.getElementById("r").value  = "";
+  document.getElementById("resultado").innerHTML = "Resultado :"
+  operacion = 2;
+}
+
+function calcular(){
+  let n = document.getElementById("n").value;
+  let r = document.getElementById("r").value;
+  if(operacion == 1){//permutaciones
+    let res = factorial(n) / (factorial(n - r));
+    document.getElementById("resultado").innerHTML = "nPr = "+res;
+    alert("Resultado :  nPr = "+res);
+  }else{//combinaciones
+    let res = factorial(n) / (factorial(r)*factorial(n - r));
+    document.getElementById("resultado").innerHTML = "nCr = "+res;
+    alert("Resultado :  nCr = "+res);
+  }
+
+}
+
+function factorial (n) {
+	var total = 1;
+	for (i=1; i<=n; i++) {
+		total = total * i;
+	}
+	return total;
 }
